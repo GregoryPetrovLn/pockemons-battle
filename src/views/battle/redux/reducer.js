d8aa272d30717d08262645b74e90f1a1279a4c78
Gregory Petrov <grishap96123@gmail.com>
2022-08-21 14:24:41 +0300

    removed unnessesary

--- src/views/battle/redux/reducer.js
+++ src/views/battle/redux/reducer.js
@@ -1,18 +1,10 @@
-import {RESET_CALC_HISTORY, SET_CALC_HISTORY, SET_CALC_RESULT} from "./actionTypes";
 
 const initialState = {
-    history: [],
-    result: ''
+
 }
 
 export const reducer = (state = initialState, {type, payload}) => {
     switch (type) {
-        case SET_CALC_HISTORY:
-            return {...state, history: [...state.history, payload]}
-        case RESET_CALC_HISTORY:
-            return {...state, history: []}
-        case SET_CALC_RESULT:
-            return {...state, result: payload}
         default:
             return state
     }
