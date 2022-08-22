aafdf5f1883dd39b501cedf6721f016c87c7edca
Gregory Petrov <grishap96123@gmail.com>
2022-08-22 11:23:57 +0300

    removed comments

--- src/views/battle/Battle.js
+++ src/views/battle/Battle.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState} from 'react';
 import './Battle.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {getListPockemons} from "./redux/actions";
 import Player from "./components/player/Player";
 import './Battle.scss'
 import TableView from "./components/table-view/TableView";
@@ -9,7 +8,6 @@ import NewGameView from "./components/new-game/NewGameView";
 import {getRandomInt} from "../../utils/service";
 import ShowWinner from "./components/show-winner/ShowWinner";
 import History from "./components/history/History";
-import {GET_RANDOM_POCKEMON} from "./redux/actionTypes";
 import axios from "axios";
 
 const Pockemon = {
@@ -26,19 +24,6 @@ const Battle = () => {
     const [isNewGame, setIsNewGame] = useState(true)
     const [lastMove, setLastMove] = useState({you: 0, opponent: 0})
     const [winner, setWinner] = useState(null)
-
-    useEffect(() => {
-        //==============================
-        //====== READ ME ===============
-        //==============================
-        //The api you provided returns an object or a list of objects that does not have an image.
-        // Logically, after such a request, the picture can be put already at the initialization of a new game.
-        // In my case, there is a static image from a file
-        //==============================
-        //==============================
-        //==============================
-        //  dispatch(getListPockemons())
-    }, [])
 
     const getRandomPockemonsImages = async () => {
         let id = getRandomInt(0, 500)
